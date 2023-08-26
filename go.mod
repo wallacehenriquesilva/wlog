98def4ecfaf3c6cf8109d5277cf04969f9c281e0
Wallace Henrique José da Silva <wallace_25@hotmail.com>
2023-08-26 15:54:49 -0300

    feat: add log functions

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/wallacehenriquesilva/wlog
+
+go 1.20
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/rs/zerolog v1.30.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+)
---
+++ wlog.go
@@ -0,0 +1,101 @@
+package wlog
+
+import (
+	"context"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/gofrs/uuid"
+	"github.com/rs/zerolog"
+	"runtime/debug"
+)
+
+const (
+	goVersionField     = "go_version"
+	correlationIDField = "correlation_id"
+)
+
+var (
+	once sync.Once
+	log  zerolog.Logger
+)
+
+func NewDefaultLogger() zerolog.Logger {
+	once.Do(func() {
+		zerolog.TimeFieldFormat = time.RFC3339Nano
+
+		buildInfo, _ := debug.ReadBuildInfo()
+
+		log = zerolog.New(os.Stdout).
+			Level(zerolog.InfoLevel).
+			With().
+			Stack().
+			Caller().
+			Timestamp().
+			Str(goVersionField, buildInfo.GoVersion).
+			Logger()
+	})
+
+	return log
+}
+
+func Debug(ctx context.Context) *zerolog.Event {
+	l := getLogger(ctx)
+
+	return l.Debug()
+}
+
+func Info(ctx context.Context) *zerolog.Event {
+	l := getLogger(ctx)
+
+	return l.Info()
+}
+
+func Warn(ctx context.Context) *zerolog.Event {
+	l := getLogger(ctx)
+
+	return l.Warn()
+}
+
+func Error(ctx context.Context) *zerolog.Event {
+	l := getLogger(ctx)
+
+	return l.Warn()
+}
+
+func getLogger(ctx context.Context) *zerolog.Logger {
+	l := zerolog.Ctx(ctx)
+
+	if l.GetLevel() == zerolog.Disabled {
+		newLog := zerolog.New(os.Stdout).
+			Level(zerolog.InfoLevel).
+			With().
+			Timestamp().
+			Logger()
+
+		newLog.UpdateContext(func(c zerolog.Context) zerolog.Context {
+			correlationID, _ := uuid.NewV4()
+
+			return c.Str(correlationIDField, correlationID.String())
+		})
+
+		return &newLog
+	}
+
+	return l
+}
+
+func AddContextString(ctx context.Context, key, value string) context.Context {
+	l := getLogger(ctx)
+
+	ctx = l.WithContext(ctx)
+
+	l = getLogger(ctx)
+
+	l.UpdateContext(func(c zerolog.Context) zerolog.Context {
+		return c.Str(key, value)
+	})
+
+	return ctx
+}
